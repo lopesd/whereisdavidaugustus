0fd8e3d3eddfa85f30a0adb94d7b3757db7dc6a9
David Lopes <thelowps@gmail.com>
2021-01-25 19:26:16 -0600

    Leaderboards!

--- buildtools/wida_build.rb
+++ buildtools/wida_build.rb
@@ -2,6 +2,7 @@ require 'erb'
 require 'fileutils'
 require 'json'
 require 'yaml'
+require 'date'
 
 module WidaBuild
   class << self
@@ -22,6 +23,21 @@ module WidaBuild
 
     CHAPTER_CONFIG_FILENAME = 'config.yaml'
 
+    # date helper
+    def formatted_duration(seconds)
+      seconds = seconds.to_i
+      days = seconds / (60 * 60 * 24)
+      hours = (seconds / (60 * 60)) % 24
+      minutes = (seconds / 60) % 60
+      seconds = seconds % 60
+
+      days_str = days > 0 ? "#{days}d " : ''
+      hours_str = hours > 0 || days > 0 ? "#{hours}h " : ''
+      minutes_str = minutes > 0 || hours > 0 || days > 0 ? "#{minutes}m " : ''
+      seconds_str = "#{seconds}s"
+      "#{days_str}#{hours_str}#{minutes_str}#{seconds_str}"
+    end
+
     # versions the filename using the given version id
     def versioned_filename(filename, build_version_id)
       extension = File.extname(filename)
@@ -34,17 +50,98 @@ module WidaBuild
       string.gsub(HTML_SANITIZE_REGEX) { |match| HTML_SANITIZE_MAP[match] }
     end
 
+    # given a hashmap { peeper => score }, return a sorted array of
+    # objects that can be used to render a leaderboard.
+    # the "increasing" flag can be used to indicate that lower scores rank higher.
+    def create_ranking_array(peeper_to_score_hash, increasing=false)
+      rankings_array = peeper_to_score_hash.to_a.sort_by { |e| e[1] }
+      rankings_array.reverse! unless increasing
+      previous_score = nil
+      previous_rank = 0
+      rankings_array.each_with_index.map do |entry, i|
+        rank = previous_rank
+        if entry[1] != previous_score
+          rank = i + 1
+          previous_rank = rank
+          previous_score = entry[1]
+        end
+        { 'peeper' => entry[0], 'score' => entry[1], 'rank' => rank }
+      end
+    end
+
+    def calculate_leaderboards(chapters, peeps)
+      leaderboards = []
+      chapters.each do |chapter|
+        next unless chapter['showLeaderboards']
+
+        # MOST
+        peeper_to_peep_count = {}
+        chapter['checkins'].each do |checkin|
+          peep = peeps[checkin['checkinId']]
+          next unless peep
+          peeper_to_peep_count[peep['peeper']] ||= 0
+          peeper_to_peep_count[peep['peeper']] += 1
+        end
+        most = create_ranking_array(peeper_to_peep_count)
+
+        # QUICKEST
+        peeper_to_quickest_peep = {}
+        chapter['checkins'].each do |checkin|
+          peep = peeps[checkin['checkinId']]
+          next unless (peep && peep['peepTime'] && checkin['uploadTime'])
+          peep_time = DateTime.parse(peep['peepTime'])
+          upload_time = DateTime.parse(checkin['uploadTime'])
+          seconds_elapsed = peep_time.to_time.to_i - upload_time.to_time.to_i
+          existing_record = peeper_to_quickest_peep[peep['peeper']]
+          if !existing_record || existing_record > seconds_elapsed
+            peeper_to_quickest_peep[peep['peeper']] = seconds_elapsed
+          end
+        end
+        quickest = create_ranking_array(peeper_to_quickest_peep, true)
+
+        # STREAK
+        peeper_to_longest_streak = {}
+        current_streaker = nil
+        current_streak_length = 0
+        chapter['checkins'].each do |checkin|
+          peep = peeps[checkin['checkinId']]
+          if peep && peep['peeper'] == current_streaker
+            current_streak_length += 1
+          else
+            if current_streaker
+              peeper_to_longest_streak[current_streaker] ||= 0
+              if peeper_to_longest_streak[current_streaker] < current_streak_length
+                peeper_to_longest_streak[current_streaker] = current_streak_length
+              end
+            end
+            current_streaker = peep && peep['peeper']
+            current_streak_length = 1
+          end
+        end
+
+        streak = create_ranking_array(peeper_to_longest_streak)
+
+        leaderboard = { 
+          'name' => chapter['name'],
+          'heading' => chapter['heading'],
+          'most' => most,
+          'quickest' => quickest,
+          'streak' => streak
+        }
+        leaderboards.push(leaderboard)
+      end
+      leaderboards
+    end
+
     # options hash should include :src_root, :build_root, :credentials_file, :checkins_dir
     def build(options)
       # create build folder if non existent
       FileUtils.mkdir_p(options[:build_root])
 
-
       ## COPY SRC INTO BUILD ##
       FileUtils.cp_r("#{options[:src_root]}/.", options[:build_root])
 
       # GET CHECKINS ORGANIZED IN CHAPTERS
-      chapters = {}
       content_config = YAML.load(File.read("#{options[:checkins_dir]}/#{CHAPTER_CONFIG_FILENAME}"))
       chapters = content_config['chapters']
       chapters.each do |chapter_config|
@@ -64,6 +161,9 @@ module WidaBuild
         peep = JSON.parse(File.read(peep_file))
         peeps[peep['checkinId']] = peep
       end
+
+      # CALCULATE LEADERBOARDS
+      leaderboards = calculate_leaderboards(chapters, peeps)
 
       ## TEMPLATING ##
       build_stage = options[:build_stage]
